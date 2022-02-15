9e7ba543bce7d1276436c76acb0520337295c61e
Breon White <bdwhite92@gmail.com>
2022-02-15 17:13:10 -0500

    Navigation: Add Event Listeners and Handlers

--- javascript/index.js
+++ javascript/index.js
@@ -21,7 +21,9 @@
 /**  Node Getters **/
 const mainDiv = () => document.getElementById('main');
 const homeLink = () => document.getElementById('home-link');
-
+const addCaptionLink = () => document.getElementById('add-caption-link');
+const searchLyricsLink = () => document.getElementById('search-lyrics-link');
+const myCaptionsLink = () => document.getElementById('my-captions-link');
 
 
 
@@ -30,11 +32,27 @@ const homeLinkHandler = () => {
     homeLink().addEventListener('click', loadHome);
 }
 
+const addCaptionLinkHandler = () => {
+    addCaptionLink().addEventListener('click', loadAddCaption);
+}
+
+const searchLyricsLinkHandler = () => {
+    searchLyricsLink().addEventListener('click', loadSearchLyrics);
+}
+
+const myCaptionsLinkHandler = () => {
+    myCaptionsLink().addEventListener('click', loadMyCaptions);
+}
+
+
 
 
 
 /**  Event Handlers **/
-const loadHome = () => {
+const loadHome = event => {
+    if(event) {
+        event.preventDefault();
+    }
     resetMainDiv();
     const h1 = document.createElement('h1')
     const p = document.createElement('p')
@@ -49,6 +67,92 @@ const loadHome = () => {
     mainDiv().appendChild(p);
 }
 
+const loadAddCaption = event => {
+    event.preventDefault();
+    resetMainDiv();
+    const h1 = document.createElement('h1');
+    h1.innerText = 'Add Caption';
+    
+    mainDiv().appendChild(h1);
+}
+
+const loadSearchLyrics = event => {
+    event.preventDefault();
+    resetMainDiv();
+    const h1 = document.createElement('h1');
+    h1.innerText = 'Search by Lyrics';
+
+    mainDiv().appendChild(h1);
+}
+
+const loadMyCaptions = event => {
+     event.preventDefault();
+     resetMainDiv();
+
+     const h1 = document.createElement('h1');
+     const captionList = document.createElement('ul');
+
+     //Caption Classnames
+     h1.innerText = 'Saved Captions';
+     captionList.className = 'collection';
+    
+    //Icons
+    let favoriteIconText = 'favorite_border';
+
+    // Create Caption Card
+    let captionCard = document.createElement('li')
+    captionCard.className = 'collection-item avatar saved-lyric-item';
+    
+        //Caption Variables
+        let captionImage = "img url here";
+        let captionLyrics = "caption lyrics here";
+        let captionSong = "caption song here";
+        let captionArtist = "caption artist here";
+
+    captionCard.innerHTML = `
+        <img src="${captionImage}" alt="" class="circle">
+        <span class="title" id="caption-list-lyric"><i>${captionLyrics}</i></span>
+              <p>
+                <label for="caption-item-song">Song:</label>
+                <span id="caption-item-song">${captionSong}</span><br>
+                <label for="caption-item-artist">Arist:</label>
+                <span id="caption-item-artist">${captionArtist}</span>
+              </p>
+              <a href="#" id="delete-lyric" class="secondary-content">
+                  <i class="material-icons">delete</i>
+                  <i class="material-icons">${favoriteIconText}</i>
+                  <i class="material-icons">content_copy</i>
+                </a>
+    `;
+    
+    captionList.appendChild(captionCard);
+
+     mainDiv().appendChild(h1);
+    mainDiv().appendChild(captionList);
+
+
+// {/* <h1>My Captions</h1>
+//          <ul class="collection">
+//             <li class="collection-item avatar">
+//               <img src="images/yuna.jpg" alt="" class="circle">
+//               <span class="title" id="caption-list-lyric"><i>"This a rollie not a stop watch, shit don't ever stop</i></span>
+//               <p>
+//                 <label for="caption-item-song">Song:</label>
+//                 <span id="caption-item-song">Non-Stop</span><br>
+//                 <label for="caption-item-artist">Arist:</label>
+//                 <span id="caption-item-artist">Drake</span>
+//               </p>
+//               <a href="#" id="delete-lyric" class="secondary-content">
+//                   <i class="material-icons">delete</i>
+//                   <i class="material-icons">favorite_border</i>
+//                   <i class="material-icons">content_copy</i>
+//                 </a>
+//             </li> */}
+
+
+
+
+};
 
 
 
@@ -65,4 +169,7 @@ document.addEventListener('DOMContentLoaded', function() {
     //1. Load the homepage
     loadHome();
     homeLinkHandler();
+    addCaptionLinkHandler();
+    searchLyricsLinkHandler();
+    myCaptionsLinkHandler();
 })
