1e9feceea2067b9fb40e2f275d6947cbcdfa0ba0
Breon White <bdwhite92@gmail.com>
2022-02-17 15:44:27 -0500

    Genius API Config to Find Lyrics

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,55 @@
 /*  Three Question Rule:
   Overview: Click the home link and make the home page appear
 
-    1. When do we want the link to be clicked and make the homepage appear?
-        Right away (domcontentloaded)
-    2. What is the cause of the event ? 
-        (Click event)
-    3. WHat's the effect? What's going to happen when the event triggers 
-        Makes the home page appear 
+    1. When? DOMCOntent Loaded When we click on my captions link, we should see all captions from the database 
+    2. Cause? DomContentLoaded
+    3. Effect? Fetch all of the saved captions
  */
 
 
 
 
 /**  Global Variables **/
+ const baseURL = 'http://localhost:3000';
+ const geniusURL = 'https://genius-song-lyrics1.p.rapidapi.com/search/multi?q=';
+ 
+ let captions = [];
+ let lyricResults = [];
 
 
+/**Helper Functions */
+Object.byString = function(o, s) {
+    s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+    s = s.replace(/^\./, '');           // strip a leading dot
+    var a = s.split('.');
+    for (var i = 0, n = a.length; i < n; ++i) {
+        var k = a[i];
+        if (k in o) {
+            o = o[k];
+        } else {
+            return;
+        }
+    }
+    return o;
+};
 
-
+function createLyricObj(obj) {
+    const lyricSnippet = Object.byString(obj, 'highlights[0].value');
+    const lyricSong = Object.byString(obj, 'result.title_with_featured');
+    const lyricArtist = Object.byString(obj, 'result.primary_artist.name');
+    const artistIMG = Object.byString(obj, 'result.primary_artist.image_url');
+        
+    let lyricOutput = {
+        "image": `${artistIMG}`,
+        "lyrics": `${lyricSnippet}`,
+        "song": `${lyricSong}`,
+        "artist": `${lyricArtist}`,
+        "custom": false,
+        "favorited": false,
+            };
+    console.log('Lyric Output:', lyricOutput);
+    return lyricOutput;
+}
 
 /**  Node Getters **/
 const mainDiv = () => document.getElementById('main');
@@ -24,6 +57,8 @@ const homeLink = () => document.getElementById('home-link');
 const addCaptionLink = () => document.getElementById('add-caption-link');
 const searchLyricsLink = () => document.getElementById('search-lyrics-link');
 const myCaptionsLink = () => document.getElementById('my-captions-link');
+const lyricSearchButton = () => document.getElementById('lyric-search-button');
+const lyricSearchInput = () => document.getElementById('lyric-search-input');
 
 
 
@@ -42,6 +77,10 @@ const searchLyricsLinkHandler = () => {
 
 const myCaptionsLinkHandler = () => {
     myCaptionsLink().addEventListener('click', loadMyCaptions);
+}
+
+const searchButtonHandler = () => {
+    lyricSearchButton().addEventListener('click', searchForLyrics);
 }
 
 
@@ -80,9 +119,35 @@ const loadSearchLyrics = event => {
     event.preventDefault();
     resetMainDiv();
     const h1 = document.createElement('h1');
+    const form = document.createElement('form');
+    const div1 = document.createElement('div');
+    
     h1.innerText = 'Search by Lyrics';
+    div1.className = 'input-field';
+
+    div1.innerHTML = `
+        <input id="lyric-search-input" type="text">
+        <label for="lyric-search-input">Enter Song Lyrics</label>
+            <button class="btn waves-effect waves-light btn-large light-blue darken-2 right" type="submit" id='lyric-search-button'>Find Lyrics
+            <i class="material-icons right">search</i>
+            </button>
+    `;
+    
+    form.appendChild(div1);
+
+    // <form>
+    //     <div class="input-field">
+    //         <input id="lyric-search-input" type="text">
+    //         <label for="lyric-search">Enter Song Lyrics</label>
+    //         <button class="btn waves-effect waves-light btn-large light-blue darken-2 right" type="submit" id='lyric-search-button'>Find Lyrics
+    //             <i class="material-icons right">search</i>
+    //         </button>
+    //     </div>  
+    // </form>
 
     mainDiv().appendChild(h1);
+    mainDiv().appendChild(form);
+    searchButtonHandler();
 }
 
 const loadMyCaptions = event => {
@@ -100,59 +165,104 @@ const loadMyCaptions = event => {
     let favoriteIconText = 'favorite_border';
 
     // Create Caption Card
-    let captionCard = document.createElement('li')
-    captionCard.className = 'collection-item avatar saved-lyric-item';
-    
-        //Caption Variables
-        let captionImage = "img url here";
-        let captionLyrics = "caption lyrics here";
-        let captionSong = "caption song here";
-        let captionArtist = "caption artist here";
+    captions.forEach(caption => {
+        const captionCard = document.createElement('li');
+        captionCard.className = 'collection-item avatar saved-lyric-item';
 
-    captionCard.innerHTML = `
-        <img src="${captionImage}" alt="" class="circle">
-        <span class="title" id="caption-list-lyric"><i>${captionLyrics}</i></span>
-              <p>
-                <label for="caption-item-song">Song:</label>
-                <span id="caption-item-song">${captionSong}</span><br>
-                <label for="caption-item-artist">Arist:</label>
-                <span id="caption-item-artist">${captionArtist}</span>
-              </p>
-              <a href="#" id="delete-lyric" class="secondary-content">
-                  <i class="material-icons">delete</i>
-                  <i class="material-icons">${favoriteIconText}</i>
-                  <i class="material-icons">content_copy</i>
-                </a>
-    `;
-    
-    captionList.appendChild(captionCard);
+         //Caption Variables
+         let captionImage = caption.image;
+         let captionLyrics = caption.lyrics;
+         let captionSong = caption.song;
+         let captionArtist = caption.artist;
 
+         captionCard.innerHTML = `
+         <img src="${captionImage}" alt="" class="circle">
+         <span class="title" id="caption-list-lyric"><i>${captionLyrics}</i></span>
+               <p>
+                 <label for="caption-item-song">Song:</label>
+                 <span id="caption-item-song">${captionSong}</span><br>
+                 <label for="caption-item-artist">Arist:</label>
+                 <span id="caption-item-artist">${captionArtist}</span>
+               </p>
+               <a href="#" id="delete-lyric" class="secondary-content">
+                    <i class="material-icons">${favoriteIconText}</i>
+                    <i class="material-icons">content_copy</i>
+                    <i class="material-icons">delete</i>
+                 </a>
+                 `;
+        
+        captionList.appendChild(captionCard);
+    })
+    
      mainDiv().appendChild(h1);
     mainDiv().appendChild(captionList);
+}
+
+const searchForLyrics = event => {
+    event.preventDefault();
+//Converts lyric search input (string) into Fetch URL 
+    const lyricUrl = generateSearchURL(lyricSearchInput().value);
+    
+//Fetch: Searches Genius API for results
+    fetch(geniusURL + lyricUrl + '&per_page=5&page=1', {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "genius-song-lyrics1.p.rapidapi.com",
+		"x-rapidapi-key": "85900b7c00mshcf991f3bb28992ep180ad5jsn97819322cdfa"
+	}
+})
+.then(resp => resp.json())
+.then(data => {
+    let lyricResults = data;
+    let hitResults = Object.byString(data, 'response.sections[2].hits');
+
+    const mapThrough = hitResults.map(data => {
+        const lyricSnippet = Object.byString(data, 'highlights[0].value');
+        const lyricSong = Object.byString(data, 'result.title_with_featured');
+        const lyricArtist = Object.byString(data, 'result.primary_artist.name');
+        const artistIMG = Object.byString(data, 'result.primary_artist.image_url');
+        
+        let lyricOutput = {
+            "image": `${artistIMG}`,
+            "lyrics": `${lyricSnippet}`,
+            "song": `${lyricSong}`,
+            "artist": `${lyricArtist}`,
+            "custom": false,
+            "favorited": false,
+        };
+        console.log('Lyrics Found:', lyricOutput);
+    })
+
+    //const mapThrough = hitResults.map(createLyricObj(hitResults));
+})
+.catch(err => {
+	console.error(err);
+});
+
+    
 
 
-// {/* <h1>My Captions</h1>
-//          <ul class="collection">
-//             <li class="collection-item avatar">
-//               <img src="images/yuna.jpg" alt="" class="circle">
-//               <span class="title" id="caption-list-lyric"><i>"This a rollie not a stop watch, shit don't ever stop</i></span>
-//               <p>
-//                 <label for="caption-item-song">Song:</label>
-//                 <span id="caption-item-song">Non-Stop</span><br>
-//                 <label for="caption-item-artist">Arist:</label>
-//                 <span id="caption-item-artist">Drake</span>
-//               </p>
-//               <a href="#" id="delete-lyric" class="secondary-content">
-//                   <i class="material-icons">delete</i>
-//                   <i class="material-icons">favorite_border</i>
-//                   <i class="material-icons">content_copy</i>
-//                 </a>
-//             </li> */}
+    //Get the text input from the lyric search field
+    //let lyricSearchInput = document.getElementById('lyric-search-input').value;
+    //console.log(lyricSearchInput);
 
+    //console.log(lyricUrl);
+    // fetch()
+    //     .then(resp => resp.json())
+    //     .then(data => {
+    //         console.log('data', data)
+    //         captions = data;
+    //     })
+}
 
-
-
-};
+/**  REQUESTS **/
+const loadCaptions = () => {
+    fetch(baseURL + '/captions')
+        .then(resp => resp.json())
+        .then(data => {
+            console.log('data', data)
+        })
+}
 
 
 
@@ -161,13 +271,19 @@ const resetMainDiv = () => {
     mainDiv().innerHTML = '';
 }
 
+const generateSearchURL = (string) => {
+    let encoded = encodeURIComponent(string);
+    return encoded;
+};
+
 
 /**  Start Up **/
 
 document.addEventListener('DOMContentLoaded', function() {
     //What do we want it to do when th epage loads?
     //1. Load the homepage
-    loadHome();
+    loadCaptions();
+    // loadHome();
     homeLinkHandler();
     addCaptionLinkHandler();
     searchLyricsLinkHandler();
